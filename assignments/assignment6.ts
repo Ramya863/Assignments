569adab5e7c29a85d38977bf81437ffdd3db15dc
Ramya <kurraramyasree@gmail.com>
2026-03-16 07:02:12 +0530

    check whether given number is prime or not.

--- assignments/assignment6.ts
+++ assignments/assignment6.ts
@@ -3,24 +3,24 @@
 let numbers: number[] = [7, 25,1];
 
 // Loop through each number in array
-for (let n of numbers) {
+for (let num of numbers) {
 
     //Assume the number is true initially
     let isPrime: boolean = true;
 
     // Check if number is less than or equal to 1
-    if (n <= 1) {
+    if (num <= 1) {
         isPrime = false;
-        console.log(n + " is not prime number");
+        console.log(num + " is not prime number");
     } else {
         // Check divisibility
-        for (let i:number = 2; i < n; i++) {
-            if (n % i == 0) {
+        for (let i:number = 2; i < num; i++) {
+            if (num % i == 0) {
                 isPrime = false;
                 break;
             }
         }
-         console.log( n + " is a prime number");
+         console.log( num + " is a prime number");
     }
 
     
